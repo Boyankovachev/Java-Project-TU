687bd307faa14a3391f631c51279b353260537ad
Boyan Kovachev <boyankovachev40@gmail.com>
2023-12-04 22:08:33 +0200

    Initial commit with data from Spring Initializer

---
+++ src/main/java/com/kursova/kursovajavatu/KursovaJavaTuApplication.java
@@ -0,0 +1,13 @@
+package com.kursova.kursovajavatu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursovaJavaTuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KursovaJavaTuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kursova/kursovajavatu/KursovaJavaTuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kursova.kursovajavatu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KursovaJavaTuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
